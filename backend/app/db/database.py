e53f7d3ee2dfa4077359f6e6cfdd85783c87ed5a
angvit <angelo.vitalino@baruchmail.cuny.edu>
2025-03-28 19:40:14 -0400

    adding enviornment variable for postgres connection

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-DATABASE_URL = ""
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 
 # managing transactions and DB state
