302eeac648b325dafacb1f0ad3349ccc95cd7972
angvit <angelo.vitalino@baruchmail.cuny.edu>
2025-04-01 23:22:38 -0400

    added fastapi endpoints

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,8 +1,12 @@
 import uvicorn 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Query, HTTPException
+from sqlalchemy.orm import Session
+from uuid import UUID
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-from typing import List
+from app.db.database import get_db
+from app.data_models.content import Content
+from app.data_models.content_item import ContentItem
+from app.schemas.content import ContentCreate, ContentRead
 
 app = FastAPI()
 
@@ -18,5 +22,36 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+
+@app.post("/content/save", response_model=ContentRead)
+def save_content(content: ContentCreate, db: Session = Depends(get_db)):
+    new_content = Content(**content.model_dump())
+    db.add(new_content)
+    db.commit()
+    db.refresh(new_content)
+    return new_content
+
+
+@app.get("/content", response_model=list[ContentRead])
+def get_user_content(user_id: UUID = Query(...), db: Session = Depends(get_db)):
+    return db.query(Content).filter(Content.user_id == user_id).all()
+
+
+app.delete("/content/{content_id}", status_code=204)
+def delete_content(content_id: UUID, user_id: UUID, db: Session=Depends(get_db)):
+    content = db.query(Content).filter(Content.content_id == content_id, Content.user_id == user_id).first()
+    if not content:
+        raise HTTPException(status_code=404, detail="Content not found or not owned by user")
+
+    db.delete(content)
+    db.commit()
+    return
+
+
+
+
+
+
 
 
---
+++ backend/app/schemas/content.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from typing import Optional
+from uuid import UUID
+
+class ContentCreate(BaseModel):
+    url: str
+    title: Optional[str] = None
+    source: Optional[str] = None
+
+
+class ContentRead(ContentCreate):
+    content_id: UUID
+
+    class Config:
+        orm_mode=True
