b08591516c02d8539617b6e57f839b31a5c427b4
angvit <angelo.vitalino@baruchmail.cuny.edu>
2025-04-02 17:25:47 -0400

    api endpoints and fixing tcolumn definitions

--- backend/app/data_models/content_item.py
+++ backend/app/data_models/content_item.py
@@ -5,7 +5,7 @@ from app.db.database import Base
 class ContentItem(Base):
     __tablename__ = "content_item"
 
-    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
-    content_id = Column(UUID(as_uuid=True), ForeignKey("content.content_id"))
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), primary_key=True)
+    content_id = Column(UUID(as_uuid=True), ForeignKey("content.content_id"), primary_key=True)
     saved_at = Column(TIMESTAMP, server_default="NOW()")
     notes = Column(String, nullable=True)
