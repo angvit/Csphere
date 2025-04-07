7aa155430619455176e427b60c6dc3afa5f86a2d
crosve <crosve.lucero73@myhunter.cuny.edu>
2025-04-06 21:59:36 -0400

    extension connected to the backend dummy api point

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -3,15 +3,18 @@ from fastapi import FastAPI, Depends, Query, HTTPException
 from sqlalchemy.orm import Session
 from uuid import UUID
 from fastapi.middleware.cors import CORSMiddleware
-from app.db.database import get_db
-from app.data_models.content import Content
-from app.data_models.content_item import ContentItem
-from app.schemas.content import ContentCreate, ContentRead
+from pydantic import BaseModel
+
+# from app.db.database import get_db
+# from app.data_models.content import Content
+# from app.data_models.content_item import ContentItem
+# from app.schemas.content import ContentCreate, ContentRead
 
 app = FastAPI()
 
 # change link later once deployed
 origins = [
+    "*",
     "http://localhost:3000"
 ]
 
@@ -23,41 +26,57 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-
-@app.post("/content/save", response_model=ContentRead)
-def save_content(content: ContentCreate, db: Session = Depends(get_db)):
-    new_content = Content(**content.model_dump())
-    db.add(new_content)
-    db.commit()
-    db.refresh(new_content)
-    return new_content
+class ContentFromUrl(BaseModel):
+    url: str
+    title: str
 
 
-# gets all content for a specific user 
-@app.get("/content", response_model=list[ContentRead])
-def get_user_content(user_id: UUID = Query(...), db: Session = Depends(get_db)):
-    return db.query(Content).filter(Content.user_id == user_id).all()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/content/saveUrl")
+def save_url(ContentFromUrl: ContentFromUrl):
+    url = ContentFromUrl.url
+    title = ContentFromUrl.title
+    
+    print("URL being saved: ", url ,"\n Title: ", title)
+    return {"url": "saved", "title": title}
+
+# @app.post("/content/save", response_model=ContentRead)
+# def save_content(content: ContentCreate, db: Session = Depends(get_db)):
+#     new_content = Content(**content.model_dump())
+#     db.add(new_content)
+#     db.commit()
+#     db.refresh(new_content)
+#     return new_content
 
 
-# gets a single piece of content for a specific user
-@app.get("/content/{content_id}", response_model=ContentRead)
-def get_piece_content(content_id: UUID, user_id: UUID = Query(...), db: Session = Depends(get_db)):
-    content = db.query(Content).filter(Content.content_id == content_id, Content.user_id == user_id).first()
-
-    if not content:
-        raise HTTPException(status_code=404, detail="Content not found for this user")
-    return content
+# # gets all content for a specific user 
+# @app.get("/content", response_model=list[ContentRead])
+# def get_user_content(user_id: UUID = Query(...), db: Session = Depends(get_db)):
+#     return db.query(Content).filter(Content.user_id == user_id).all()
 
 
-app.delete("/content/{content_id}", status_code=204)
-def delete_content(content_id: UUID, user_id: UUID, db: Session=Depends(get_db)):
-    content = db.query(Content).filter(Content.content_id == content_id, Content.user_id == user_id).first()
-    if not content:
-        raise HTTPException(status_code=404, detail="Content not found or not owned by user")
+# # gets a single piece of content for a specific user
+# @app.get("/content/{content_id}", response_model=ContentRead)
+# def get_piece_content(content_id: UUID, user_id: UUID = Query(...), db: Session = Depends(get_db)):
+#     content = db.query(Content).filter(Content.content_id == content_id, Content.user_id == user_id).first()
 
-    db.delete(content)
-    db.commit()
-    return
+#     if not content:
+#         raise HTTPException(status_code=404, detail="Content not found for this user")
+#     return content
+
+
+# app.delete("/content/{content_id}", status_code=204)
+# def delete_content(content_id: UUID, user_id: UUID, db: Session=Depends(get_db)):
+#     content = db.query(Content).filter(Content.content_id == content_id, Content.user_id == user_id).first()
+#     if not content:
+#         raise HTTPException(status_code=404, detail="Content not found or not owned by user")
+
+#     db.delete(content)
+#     db.commit()
+#     return
 
 
 
