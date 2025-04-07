91580c6f0d0d895026b31e5d04f0d3af2c389b90
crosve <crosve.lucero73@myhunter.cuny.edu>
2025-04-06 21:05:27 -0400

    grabbing the youtueb link

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,6 +1,20 @@
-document.getElementById("bookMarkBtn").addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  if (tab && tab.url) {
-    console.log("Bookmarking URL: ", tab.url);
+alert("Popup script loaded!");
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("bookMarkBtn");
+  console.log("Popup script loaded and running...");
+  console.log("Button element: ", btn);
+  if (btn) {
+    btn.addEventListener("click", async () => {
+      let [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+      });
+      console.log("popup.js running...");
+      if (tab && tab.url) {
+        console.log("Bookmarking URL: ", tab.url);
+      }
+    });
+  } else {
+    console.error("Button with ID 'bookMarkBtn' not found.");
   }
 });
