f1f3d7584b2bd7aac7032be0c00c14589d732bc0
crosve <crosve.lucero73@myhunter.cuny.edu>
2025-04-12 14:05:17 -0400

    added login and shadcn stuff as well

---
+++ frontend/src/app/components/LoginForm.tsx
@@ -0,0 +1,89 @@
+"use client";
+import { useState } from "react";
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  name_6208164905: z.string().min(1).min(5).max(50),
+  name_0162320061: z.string(),
+});
+
+export default function LoginForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      console.log(values);
+      toast(
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      );
+    } catch (error) {
+      console.error("Form submission error", error);
+      toast.error("Failed to submit the form. Please try again.");
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-6 p-6 rounded-2xl w-full max-w-lg bg-gray-800 "
+      >
+        <FormField
+          control={form.control}
+          name="name_6208164905"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="username" type="text" {...field} />
+              </FormControl>
+              <FormDescription>Enter your username</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="name_0162320061"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="••••••••"
+                  className="pl-10 bg-gray-700 border-gray-600 text-white focus:border-blue-400"
+                />
+              </FormControl>
+              <FormDescription>Enter your password.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,93 +3,33 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      {" "}
+      <main className="h-screen w-screen flex items-center justify-center bg-gray-900 bg-gradient-to-br from-gray-900 to-gray-800 text-white relative">
+        <div className="flex flex-col items-center justify-center w-10/12 md:w-8/12 h-full space-y-10 gap-10">
+          <section className="max-w-3xl">
+            <h1 className="text-5xl md:text-6xl font-bold text-center text-white  mb-8">
+              Innovate Faster with
+              <span className="block mt-2">Amane Soft</span>
+            </h1>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            <p className="text-xl text-center text-gray-300 mb-10 max-w-2xl mx-auto">
+              Empowering businesses with cutting-edge software solutions. From
+              AI-driven analytics to seamless cloud integrations, we're shaping
+              the future of technology.
+            </p>
+          </section>
+
+          <section className="flex  justify-center h-[30px] gap-6 w-[300px] md:w-[400px] lg:w-[500px]">
+            <button className="bg-white text-gray-900 px-6 py-3 rounded-lg h-auto w-full text-center  hover:bg-gray-100 transition duration-300 ">
+              Explore Solutions
+            </button>
+            <button className="text-white leading-2 px-6 py-3 w-full text-center hover:text-gray-200 transition duration-300 bg-black rounded-lg">
+              Schedule a Demo
+            </button>
+          </section>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
