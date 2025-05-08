f1da984233e86d519a13c159963c46111a99fb14
angvit <angelo.vitalino@baruchmail.cuny.edu>
2025-05-08 00:37:50 -0400

    fixing search bar styling

--- frontend/src/components/SearchInput.tsx
+++ frontend/src/components/SearchInput.tsx
@@ -14,23 +14,18 @@ export default function SearchInput({ onSearch }: Props) {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="relative w-full sm:max-w-2xl mx-auto mb-8"
-    >
+    <div className="relative w-full sm:max-w-2xl mx-auto mb-8">
       <input
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
         type="text"
         placeholder="Search..."
         className="w-full px-4 py-2 rounded-full border-[0.5px] border-black text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-600"
       />
       <button
         type="submit"
-        className="absolute right-3 top-1/2 transform -translate-y-1/2"
+        className="absolute inset-y-0 right-3 flex items-center"
       >
         <Search className="h-5 w-5 text-gray-500" />
       </button>
-    </form>
+    </div>
   );
 }
