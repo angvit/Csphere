deb3ee69d2c1a20c5b060dc409ac8553b8fb5533
angvit <angelo.vitalino@baruchmail.cuny.edu>
2025-05-08 00:37:50 -0400

    fixing search bar styling

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,9 +1,7 @@
 import { Search } from "lucide-react";
-import BookmarksPage from "@/components/BookmarksPage";
-import CategoryFilter from "@/components/category-filter";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import UserProfileSection from "@/components/user-profile-section";
+import BookmarksPage from "@/components/BookmarksPage";
 
 function page() {
   return (
