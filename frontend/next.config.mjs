140ec9982418483eed0b4e6e6687837d8b12ff49
crosve <crosve.lucero73@myhunter.cuny.edu>
2025-07-07 19:31:34 -0400

    config changes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   env: {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
