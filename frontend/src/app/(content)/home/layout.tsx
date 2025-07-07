ac7990b14db37ab218366a62b94ba494306be3c1
crosve <crosve.lucero73@myhunter.cuny.edu>
2025-07-07 16:57:20 -0400

    new BookmarkLayout wrapper

--- frontend/src/app/(content)/home/layout.tsx
+++ frontend/src/app/(content)/home/layout.tsx
@@ -6,16 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="min-h-screen w-full bg-gray-50">
       <main className="w-full">
         <div className="flex flex-col gap-8 md:flex-row">
-          <div className="flex-1 overflow-visible relative z-0">
-            <div className="flex items-center justify-between mb-6 z-10 relative">
-              <h1 className="md:text-2xl text-lg font-bold">Your Bookmarks</h1>
-              <div className="flex items-center gap-2">
-                <LatestButton />
-                <UnreadButton />
-              </div>
-            </div>
-            {children}
-          </div>
+          <div className="flex-1 overflow-visible relative z-0">{children}</div>
         </div>
       </main>
     </div>
