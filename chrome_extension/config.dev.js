23499985d70dd4e235158e4b1f0fe887e85bf674
crosve <crosve.lucero73@myhunter.cuny.edu>
2025-07-08 11:05:45 -0400

    some config changes

--- chrome_extension/config.dev.js
+++ chrome_extension/config.dev.js
@@ -1,7 +1,7 @@
-export const BACKEND_URL = "http://127.0.0.1:8000";
-export const FRONTEND_URL = "http://localhost:3000";
+// export const BACKEND_URL = "http://127.0.0.1:8000";
+// export const FRONTEND_URL = "http://localhost:3000";
 
-// export const BACKEND_URL =
-//   "https://pacific-citadel-30742-a7291f1f6593.herokuapp.com";
-// export const FRONTEND_URL = "https://csphere-nly9.vercel.app";
+export const BACKEND_URL =
+  "https://pacific-citadel-30742-a7291f1f6593.herokuapp.com";
+export const FRONTEND_URL = "https://csphere-nly9.vercel.app";
 export const DEPLOYED = true;
