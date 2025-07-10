97f62937a05f5b4876c33c193f5c20ab58c390c9
crosve <crosve.lucero73@myhunter.cuny.edu>
2025-07-09 21:08:06 -0400

    alembic logic and folder schema

---
+++ backend/app/data_models/folder.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, TIMESTAMP, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+from app.db.database import Base
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+import uuid
+
+
+class folder(Base):
+    __tablename__ = "folder"
+
+    folder_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id",  ondelete="CASCADE"), nullable=False)
+    parent_id = Column(UUID(as_uuid=True), ForeignKey("folder.folder_id",  ondelete="CASCADE"), nullable=False)
+    folder_name = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP, server_default="NOW()")
+
