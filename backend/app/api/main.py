0cd8ef90dbc4ede2e7a9ec2f07e1c7399dac60fd
crosve <crosve.lucero73@myhunter.cuny.edu>
2025-07-24 16:30:03 -0400

    adding in logegrs for the app

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os 
+import logging
+import sys
+
 
 from app.routes import user_router, folder_router, auth_router, content_router, setting_router
 
@@ -11,6 +14,18 @@ load_dotenv()
 
 
 app = FastAPI()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# StreamHandler
+stream_handler = logging.StreamHandler(sys.stdout)
+log_formatter = logging.Formatter("%(asctime)s [%(processName)s: %(process)d] [%(threadName)s: %(thread)d] [%(levelname)s] %(name)s: %(message)s")
+stream_handler.setFormatter(log_formatter)
+logger.addHandler(stream_handler)
+
+logger.info('API is starting up')
+
 
 # Update CORS origins
 origins = ["*"]
