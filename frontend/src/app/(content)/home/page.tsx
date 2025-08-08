3f70aba97559888b27c1c80bbf03cc965eb53aa7
Crosve Lucero <92947897+crosve@users.noreply.github.com>
2025-08-08 15:17:52 -0400

    fixed issue where recents don't show up if not in a folder

--- frontend/src/app/(content)/home/page.tsx
+++ frontend/src/app/(content)/home/page.tsx
@@ -4,7 +4,6 @@ interface BookmarkListProps {
   activeTab?: string;
 }
 const Page = ({ activeTab }: BookmarkListProps) => {
-  console.log("active tab in first pass for page: ", activeTab);
   return <BookmarksPage activeTab={activeTab} />;
 };
 
