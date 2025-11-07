624e81d6ee32b76065aa26e8aa7878148ee6e7a8
Crosve Lucero <92947897+crosve@users.noreply.github.com>
2025-11-06 19:42:05 -0500

    filter on frontend side is working

--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -7,28 +7,59 @@ interface ChildProps {
   setChoosenCategories: React.Dispatch<React.SetStateAction<string[]>>;
 }
 
+interface CategoryButtonProps {
+  category_name: string;
+  choosenCategories: string[];
+  setChoosenCategories: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+const CategoryButton: React.FC<CategoryButtonProps> = ({
+  category_name,
+  choosenCategories,
+  setChoosenCategories,
+}) => {
+  const [selected, setSelected] = useState(false);
+
+  const handleAppend = () => {
+    const exists = choosenCategories.includes(category_name);
+    if (!exists) {
+      setChoosenCategories((prev) => [...prev, category_name]);
+    } else {
+      //Need to remove the category
+      setChoosenCategories((prev) =>
+        prev.filter(function (category) {
+          return category !== category_name;
+        })
+      );
+    }
+    setSelected((prev) => !prev);
+  };
+
+  return (
+    <button
+      onClick={handleAppend}
+      className={`px-3 py-1 rounded-full border-[0.5px] border-black text-sm text-black hover:bg-gray-100 transition-colors
+      ${selected ? "bg-gray-100" : "bg-gray-300"}`}
+    >
+      {category_name}
+    </button>
+  );
+};
+
 const CategoryFilter: React.FC<ChildProps> = ({
   choosenCategories,
   categories,
   setChoosenCategories,
 }) => {
-  const handleAppend = (category_name: string) => {
-    const exist = choosenCategories.some((item) => category_name === item);
-
-    if (!exist) {
-      setChoosenCategories((prev) => [...prev, category_name]);
-    }
-  };
   return (
     <div className="flex flex-wrap gap-2 justify-start mb-8">
-      {categories?.map((category, index) => (
-        <button
+      {categories?.map((category, index: number) => (
+        <CategoryButton
           key={index}
-          onClick={() => handleAppend(category.category_name)}
-          className="px-3 py-1 rounded-full border-[0.5px] border-black text-sm text-black hover:bg-gray-100 transition-colors bg-gray-300"
-        >
-          {category.category_name}
-        </button>
+          category_name={category.category_name}
+          choosenCategories={choosenCategories}
+          setChoosenCategories={setChoosenCategories}
+        />
       ))}
     </div>
   );
